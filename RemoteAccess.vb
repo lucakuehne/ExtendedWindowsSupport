df910fa36ec015ab91f15bad493285f2a889062d
Luca Kühne <luca.kuehne@gmail.com>
2021-07-08 07:55:58 +0200

    tmp for merge

--- RemoteAccess.vb
+++ RemoteAccess.vb
@@ -1,3 +1,14 @@
 ﻿Public Class RemoteAccess
+    Public Computers As Array = {"LS1.softcash.ch", "LS2.softcash.ch"}
+
+    Private Sub RemoteAccess_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each Computer As String In Computers
+            CheckIfOnline(Computer)
+        Next
+    End Sub
+
+    Private Sub CheckIfOnline(Computer As String)
+
+    End Sub
 
 End Class
