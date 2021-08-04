30928958b321dcfe6034b143802588affbe7b13b
Luca Kühne <luca.kuehne@outlook.com>
2021-08-04 16:43:15 +0200

    Fertigstellung der Messaging-Funktion

--- Messaging.Designer.vb
+++ Messaging.Designer.vb
@@ -35,18 +35,18 @@ Partial Class Messaging
         'Label_AvailableTargets
         '
         Me.Label_AvailableTargets.AutoSize = True
-        Me.Label_AvailableTargets.Location = New System.Drawing.Point(11, 13)
+        Me.Label_AvailableTargets.Location = New System.Drawing.Point(10, 10)
         Me.Label_AvailableTargets.Name = "Label_AvailableTargets"
-        Me.Label_AvailableTargets.Size = New System.Drawing.Size(122, 20)
+        Me.Label_AvailableTargets.Size = New System.Drawing.Size(95, 15)
         Me.Label_AvailableTargets.TabIndex = 2
         Me.Label_AvailableTargets.Text = "Available Targets"
         '
         'Label_SelectedTargets
         '
         Me.Label_SelectedTargets.AutoSize = True
-        Me.Label_SelectedTargets.Location = New System.Drawing.Point(510, 13)
+        Me.Label_SelectedTargets.Location = New System.Drawing.Point(446, 10)
         Me.Label_SelectedTargets.Name = "Label_SelectedTargets"
-        Me.Label_SelectedTargets.Size = New System.Drawing.Size(117, 20)
+        Me.Label_SelectedTargets.Size = New System.Drawing.Size(91, 15)
         Me.Label_SelectedTargets.TabIndex = 5
         Me.Label_SelectedTargets.Text = "Selected Targets"
         '
@@ -54,60 +54,67 @@ Partial Class Messaging
         '
         Me.Button_AddTarget.Enabled = False
         Me.Button_AddTarget.Font = New System.Drawing.Font("Segoe UI", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Button_AddTarget.Location = New System.Drawing.Point(440, 42)
+        Me.Button_AddTarget.Location = New System.Drawing.Point(385, 33)
+        Me.Button_AddTarget.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button_AddTarget.Name = "Button_AddTarget"
-        Me.Button_AddTarget.Size = New System.Drawing.Size(64, 164)
+        Me.Button_AddTarget.Size = New System.Drawing.Size(56, 129)
         Me.Button_AddTarget.TabIndex = 6
         Me.Button_AddTarget.Text = ">"
         Me.Button_AddTarget.UseVisualStyleBackColor = True
         '
         'TreeView_AvailableTargets
         '
-        Me.TreeView_AvailableTargets.Location = New System.Drawing.Point(11, 37)
+        Me.TreeView_AvailableTargets.Location = New System.Drawing.Point(10, 29)
+        Me.TreeView_AvailableTargets.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.TreeView_AvailableTargets.Name = "TreeView_AvailableTargets"
-        Me.TreeView_AvailableTargets.Size = New System.Drawing.Size(422, 442)
+        Me.TreeView_AvailableTargets.Size = New System.Drawing.Size(370, 350)
         Me.TreeView_AvailableTargets.TabIndex = 7
         '
         'TreeView_SelectedTargets
         '
-        Me.TreeView_SelectedTargets.Location = New System.Drawing.Point(510, 37)
+        Me.TreeView_SelectedTargets.Location = New System.Drawing.Point(446, 29)
+        Me.TreeView_SelectedTargets.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.TreeView_SelectedTargets.Name = "TreeView_SelectedTargets"
-        Me.TreeView_SelectedTargets.Size = New System.Drawing.Size(422, 353)
+        Me.TreeView_SelectedTargets.Size = New System.Drawing.Size(370, 280)
         Me.TreeView_SelectedTargets.TabIndex = 8
         '
         'Button_RemoveTarget
         '
         Me.Button_RemoveTarget.Enabled = False
         Me.Button_RemoveTarget.Font = New System.Drawing.Font("Segoe UI", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Button_RemoveTarget.Location = New System.Drawing.Point(439, 221)
+        Me.Button_RemoveTarget.Location = New System.Drawing.Point(384, 174)
+        Me.Button_RemoveTarget.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button_RemoveTarget.Name = "Button_RemoveTarget"
-        Me.Button_RemoveTarget.Size = New System.Drawing.Size(64, 164)
+        Me.Button_RemoveTarget.Size = New System.Drawing.Size(56, 129)
         Me.Button_RemoveTarget.TabIndex = 9
         Me.Button_RemoveTarget.Text = "<"
         Me.Button_RemoveTarget.UseVisualStyleBackColor = True
         '
         'Button_SendMessage
         '
-        Me.Button_SendMessage.Location = New System.Drawing.Point(842, 396)
+        Me.Button_SendMessage.Enabled = False
+        Me.Button_SendMessage.Location = New System.Drawing.Point(737, 313)
+        Me.Button_SendMessage.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button_SendMessage.Name = "Button_SendMessage"
-        Me.Button_SendMessage.Size = New System.Drawing.Size(90, 83)
+        Me.Button_SendMessage.Size = New System.Drawing.Size(79, 66)
         Me.Button_SendMessage.TabIndex = 10
         Me.Button_SendMessage.Text = "Send"
         Me.Button_SendMessage.UseVisualStyleBackColor = True
         '
         'RichTextBox_Message
         '
-        Me.RichTextBox_Message.Location = New System.Drawing.Point(511, 397)
+        Me.RichTextBox_Message.Location = New System.Drawing.Point(447, 313)
+        Me.RichTextBox_Message.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.RichTextBox_Message.Name = "RichTextBox_Message"
-        Me.RichTextBox_Message.Size = New System.Drawing.Size(325, 82)
+        Me.RichTextBox_Message.Size = New System.Drawing.Size(285, 66)
         Me.RichTextBox_Message.TabIndex = 11
         Me.RichTextBox_Message.Text = ""
         '
         'Messaging
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(944, 491)
+        Me.ClientSize = New System.Drawing.Size(826, 388)
         Me.Controls.Add(Me.RichTextBox_Message)
         Me.Controls.Add(Me.Button_SendMessage)
         Me.Controls.Add(Me.Button_RemoveTarget)
@@ -116,6 +123,7 @@ Partial Class Messaging
         Me.Controls.Add(Me.Button_AddTarget)
         Me.Controls.Add(Me.Label_SelectedTargets)
         Me.Controls.Add(Me.Label_AvailableTargets)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Messaging"
         Me.Text = "Messaging"
         Me.ResumeLayout(False)
--- Messaging.vb
+++ Messaging.vb
@@ -31,6 +31,8 @@ Public Class Messaging
                 currentLine += 1
             End While
         Next
+        TreeView_AvailableTargets.ExpandAll()
+        TreeView_SelectedTargets.ExpandAll()
     End Sub
 
     Private Sub TreeView_AvailableTargets_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView_AvailableTargets.AfterSelect
@@ -39,6 +41,63 @@ Public Class Messaging
         Else
             Button_AddTarget.Enabled = True
         End If
+    End Sub
+
+    Private Sub TreeView_SelectedTargets_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView_SelectedTargets.AfterSelect
+        If e.Node.Level = 0 Then
+            Button_RemoveTarget.Enabled = False
+        Else
+            Button_RemoveTarget.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button_AddTarget_Click(sender As Object, e As EventArgs) Handles Button_AddTarget.Click
+        TreeView_SelectedTargets.Nodes.Add(TreeView_AvailableTargets.SelectedNode.Parent.Name, TreeView_AvailableTargets.SelectedNode.Parent.Text)
+        TreeView_SelectedTargets.Nodes.Item(TreeView_AvailableTargets.SelectedNode.Parent.Name).Nodes.Add(TreeView_AvailableTargets.SelectedNode.Name, TreeView_AvailableTargets.SelectedNode.Text)
+        TreeView_AvailableTargets.SelectedNode.Parent.Remove()
+        Button_AddTarget.Enabled = False
+        TreeView_SelectedTargets.ExpandAll()
+        If TreeView_SelectedTargets.Nodes.Count > 0 Then
+            Button_SendMessage.Enabled = True
+        Else
+            Button_SendMessage.Enabled = False
+        End If
+        TreeView_AvailableTargets.Sort()
+        TreeView_SelectedTargets.Sort()
+    End Sub
+
+    Private Sub Button_RemoveTarget_Click(sender As Object, e As EventArgs) Handles Button_RemoveTarget.Click
+        TreeView_AvailableTargets.Nodes.Add(TreeView_SelectedTargets.SelectedNode.Parent.Name, TreeView_SelectedTargets.SelectedNode.Parent.Text)
+        TreeView_AvailableTargets.Nodes.Item(TreeView_SelectedTargets.SelectedNode.Parent.Name).Nodes.Add(TreeView_SelectedTargets.SelectedNode.Name, TreeView_SelectedTargets.SelectedNode.Text)
+        TreeView_SelectedTargets.SelectedNode.Parent.Remove()
+        Button_RemoveTarget.Enabled = False
+        TreeView_AvailableTargets.ExpandAll()
+        If TreeView_SelectedTargets.Nodes.Count > 0 Then
+            Button_SendMessage.Enabled = True
+        Else
+            Button_SendMessage.Enabled = False
+        End If
+        TreeView_AvailableTargets.Sort()
+        TreeView_SelectedTargets.Sort()
+    End Sub
+
+    Private Sub Button_SendMessage_Click(sender As Object, e As EventArgs) Handles Button_SendMessage.Click
+        For Each computerNode As TreeNode In TreeView_SelectedTargets.Nodes
+            For Each sessionNode As TreeNode In computerNode.Nodes
+                Dim sessionColumns As String() = sessionNode.Text.Replace("(", "").Replace(")", "").Split(" ")
+                MsgBox(sessionColumns(5))
+                Dim messageQuery As New Process With {
+                    .StartInfo = New ProcessStartInfo With {
+                        .FileName = "cmd.exe",
+                        .Arguments = "/c msg " & sessionColumns(5) & " /SERVER:" & computerNode.Name & " '" & RichTextBox_Message.Text & "'",
+                        .CreateNoWindow = True,
+                        .RedirectStandardOutput = True
+                    }
+                }
+                messageQuery.Start()
+            Next
+        Next
+        RichTextBox_Message.Text = ""
     End Sub
 
 End Class
